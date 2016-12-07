5684cbc556b5ace6bde0994d42534b6d166aedf6
Unknown <pratham@whitelogic.in>
2016-12-07 19:43:18 +0530

    added time_to_fire feature

--- server/task/consumers.py
+++ server/task/consumers.py
@@ -2,9 +2,8 @@ from django.http import HttpResponse
 from ws4redis.redis_store import RedisMessage
 from ws4redis.publisher import RedisPublisher
 from random import randint
-random=randint(0,9999999999)
-def caller():
-        welcome = RedisMessage(str(random))  # create a welcome message to be sent to everybody
-        RedisPublisher(facility='foobar', broadcast=True).publish_message(welcome)
-        return HttpResponse('ok')
 
+def caller():
+        random=randint(0,9999999999)
+        welcome = RedisMessage(str(random))  # create a welcome message to be sent to everybody
+        RedisPublisher(facility='chatchannel', broadcast=True).publish_message(welcome)
